73e58a379312582694b5c0cdbf81aa2357589d99
BenGephardt <bengephardt@gmail.com>
2025-06-27 19:16:26 +0000

    Fix first name question

--- input_and_output/input_output_exercise_1/greeter.js
+++ input_and_output/input_output_exercise_1/greeter.js
@@ -1,4 +1,4 @@
 let readlineSync = require('readline-sync');
-let firstName = readlineSync.question('What is your name? ');
+let firstName = readlineSync.question('What is your first name? ');
 let lastName = readlineSync.question('What is your last name? ');
 console.log(`Hello, ${firstName} ${lastName}!`);
