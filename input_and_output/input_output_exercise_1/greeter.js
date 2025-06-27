0ade8a50c05a3559a8c1bc160817f42fb9ccdecb
BenGephardt <bengephardt@gmail.com>
2025-06-27 19:08:54 +0000

    Add space at end of question string

--- input_and_output/input_output_exercise_1/greeter.js
+++ input_and_output/input_output_exercise_1/greeter.js
@@ -1,3 +1,3 @@
 let readlineSync = require('readline-sync');
-let name = readlineSync.question('What is your name?');
+let name = readlineSync.question('What is your name? ');
 console.log(`Hello, ${name}!`);
