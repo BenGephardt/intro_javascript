2b91eea093d47c96d14a75ac3229821080318635
BenGephardt <bengephardt@gmail.com>
2025-06-28 19:39:42 +0000

    Add functions exercise 6 solution

---
+++ functions/functions_exercise_6.js
@@ -0,0 +1,15 @@
+// the function arguments: 2, 3, 4
+// the function body: { let result = num1 * num2 * num3; return result; }
+// the function declaration: function multiplyNumbers(num1, num2, num3) {}
+// the function invocation: multiplyNumbers(2, 3, 4)
+// the function name: multiplyNumbers
+// the function parameters: num1, num2, num3
+// the function return value: result/24
+// the names of all variables in this program: multiplyNumbers, num1, num2, num3, result, product
+
+function multiplyNumbers(num1, num2, num3) {
+  let result = num1 * num2 * num3;
+  return result;
+}
+
+let product = multiplyNumbers(2, 3, 4);
