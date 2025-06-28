76fd6793cd6899a643ecd689471b027f4ce574b7
BenGephardt <bengephardt@gmail.com>
2025-06-28 20:00:02 +0000

    Add functions exercise 11 solution

---
+++ functions/function_exercise_11.js
@@ -0,0 +1,16 @@
+function multiply(left, right) {
+  let product = left * right;
+  return product;
+}
+
+function getNumber(prompt) {
+  return parseFloat(question(prompt));
+}
+
+let left = getNumber('Enter the first number: ');
+let right = getNumber('Enter the second number: ');
+console.log(`${left} * ${right} = ${multiply(left, right)}`);
+
+// They are different variables. 
+// The variables on lines 1 and two are local parameters.
+// The variables on lines 10-12 are global variables.
