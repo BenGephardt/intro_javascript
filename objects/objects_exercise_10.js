53305227d88a9884720a716548df6652c5a4e470
BenGephardt <bengephardt@gmail.com>
2025-07-08 20:14:35 +0000

    Add objects exercise 10 solution

---
+++ objects/objects_exercise_10.js
@@ -0,0 +1,8 @@
+[1, 2, ["a", ["b", false]], null, {}]
+
+// The primitive values are 1, 2, a, b, false, null
+// {} is an object
+// [b and false] is a nested array, and is an object
+// ["a", ["b", false]] is a nested array, and is an object
+// [1, 2, ["a", ["b", false]], null, {}] is an array and an object
+// There are 6 primitive vlaues and 4 objects
