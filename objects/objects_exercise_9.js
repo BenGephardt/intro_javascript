351bd1ed99f1efa357b491cb9920572aaac95d47
BenGephardt <bengephardt@gmail.com>
2025-07-08 20:07:27 +0000

    Add correct objects exercise 9 solution

--- objects/objects_exercise_9.js
+++ objects/objects_exercise_9.js
@@ -1,29 +1,16 @@
-let objToCopy = {
-  foo: 1,
-  bar: 2,
-  qux: 3,
+let foo = {
+  a: 'hello',
+  b: 'world',
 };
 
-let copyObj = (sourceObject, keys) => {
-  let destinationObject = {};
+let qux = 'hello';
 
-  if (keys) {
-    keys.forEach(function(key) {
-      destinationObject[key] = sourceObject[key];
-    });
+function bar(argument1, argument2) {
+  argument1.a = 'hi';
+  argument2 = 'hi';
+}
 
-    return destinationObject;
-  } else {
-    return Object.assign(destinationObject, sourceObject);
-  }
-};
+bar(foo, qux);
 
-
-let newObj = copyObj(objToCopy);
-console.log(newObj);        // => { foo: 1, bar: 2, qux: 3 }
-
-let newObj2 = copyObj(objToCopy, [ 'foo', 'qux' ]);
-console.log(newObj2);       // => { foo: 1, qux: 3 }
-
-let newObj3 = copyObj(objToCopy, [ 'bar' ]);
-console.log(newObj3);       // => { bar: 2 }
+console.log(foo.a); // logs hi since objects are mutable
+console.log(qux); // logs hellow since primitives are immutable
