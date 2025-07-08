845c49c4a7125fd63b87e310ee3749b026a8e449
BenGephardt <bengephardt@gmail.com>
2025-07-08 22:03:08 +0000

    Add more stuff exercise 10 solution

---
+++ more_stuff/more_stuff_exercise_10.js
@@ -0,0 +1,6 @@
+let y = "5";
+y++;
+
+console.log(y);
+
+// y would be 6 due to automatic type coercion, the return value is still 5 however, since ++ returns the original value, not the incremeneted value
